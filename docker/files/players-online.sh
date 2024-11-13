725018f8b4327c411c69855139e1b01e4ca28be2
Tobias Tangemann <tobias@tangemann.org>
2024-11-13 22:47:55 +0100

    Clarify return code

--- docker/files/players-online.sh
+++ docker/files/players-online.sh
@@ -5,7 +5,7 @@ ONLINE_COUNT=$(echo "$PLAYERS" | grep -c " (online)$")
 
 if [[ "$ONLINE_COUNT" -gt "0" ]]; then
     echo "$PLAYERS"
-    # exit with 75 (EX_TEMPFAIL) for watchtower
+    # exit with 75 (EX_TEMPFAIL) so watchtower skips the update
     # https://containrrr.dev/watchtower/lifecycle-hooks/
     exit 75
 fi
